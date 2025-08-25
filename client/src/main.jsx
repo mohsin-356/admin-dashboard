6707dd25ef21aeb5f038ec2bd19d71ddbfac34b8
mohsin-356 <mohsinsaeed356@gmail.com>
2025-08-25 15:21:37 +0500

    feat: integrate Redux store with global state management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { configureStore } from '@reduxjs/toolkit'
+import globalReducer from "state";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
+
+import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
