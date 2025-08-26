03dbf930a67061f1cacd769b6a584357edc068f5
mohsin-356 <mohsinsaeed356@gmail.com>
2025-08-26 15:53:58 +0500

    refactor: update Navbar component to include profile photo and mode toggle imports

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ import {
 import FlexBetween from './FlexBetween';
 // import { setMode } from '../state/index.js';
 // import { useDispatch } from 'react-redux';
-// import profilePhoto from "../assets/profile-photo.jpg";
+// import profileImage from "../assets/profile-photo.jpg";
 const Navbar = () => {
   return (
     <div>Navbar</div>
